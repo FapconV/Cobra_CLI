310e67614867dcbd5b3d6b70e421f8e2edb2b9be
FapconV <chakkavenu@gmail.com>
2022-01-08 19:36:41 +0530

    venu

--- cmd/client.go
+++ cmd/client.go
@@ -93,6 +93,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		intargument, _ := strconv.ParseInt(args[0], 10, 0)
+		fmt.Println(int(intargument))
 		// Set up a connection to the server.
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
